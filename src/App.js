b3194ce8568d5a72a7749a537c85efa6f76154c3
chetna1913195 <saxenachetna30@gmail.com>
2021-05-25 09:48:35 +0530

    state added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
+import './styles/node.scss';
+import Board from './components/Board';
 const App = () => {
   return (
-    <>
-      <h1>Welcome to React Parcel Micro App!</h1>
-      <p>Hard to get more minimal than this React app.</p>
-      <small>hello this is me</small>
-    </>
+    <div className="app">
+      <h1>TIC TAC TOE!</h1>
+      <Board></Board>
+    </div>
   );
 };
 export default App;

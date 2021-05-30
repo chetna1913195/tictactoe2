66856e9800d9b126d931f3b7838945f3cf6c0089
chetna1913195 <saxenachetna30@gmail.com>
2021-05-30 10:24:44 +0530

    history added and displayed

--- src/App.js
+++ src/App.js
@@ -1,38 +1,52 @@
 import React, { useState } from 'react';
 import './styles/node.scss';
 import Board from './components/Board';
+import History from './components/History';
 import { calculateWinner } from './helper';
 const App = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
-  const winner = calculateWinner(board);
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isXNext: true },
+  ]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const current = history[currentMove];
+  console.log('history', history);
+
+  const winner = calculateWinner(current.board);
   const message = winner
     ? `winner is ${winner}
 `
-    : `next player is ${isXNext ? 'X' : 'O'}`;
+    : `next player is ${current.isXNext ? 'X' : 'O'}`;
   const handleSquareClick = position => {
-    if (board[position] || winner) {
+    if (current.board[position] || winner) {
       return;
     }
 
-    setBoard(prev => {
-      return prev.map((square, pos) => {
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
+      const newBoard = last.board.map((square, pos) => {
         if (pos === position) {
-          return isXNext ? 'X' : 'O';
+          return last.isXNext ? 'X' : 'O';
         }
 
         return square;
       });
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
     });
-
-    setIsXNext(prev => !prev);
+    setCurrentMove(prev => prev + 1);
+  };
+  const moveTo = move => {
+    setCurrentMove(move);
   };
 
   return (
     <div className="app">
       <h1>TIC TAC TOE!</h1>
       <h4>{message}</h4>
-      <Board board={board} handleSquareClick={handleSquareClick}></Board>
+      <Board
+        board={current.board}
+        handleSquareClick={handleSquareClick}
+      ></Board>
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 };
